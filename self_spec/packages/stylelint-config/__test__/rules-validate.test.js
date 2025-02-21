59ad9268fee538cccd41f8373287aab39295e2e6
xiongshengzhi <1220718650@qq.com>
2025-02-21 21:47:51 +0800

    [Feat] update the unit test

--- self_spec/packages/stylelint-config/__test__/rules-validate.test.js
+++ self_spec/packages/stylelint-config/__test__/rules-validate.test.js
@@ -22,4 +22,64 @@ describe("test/rules-validate.test.js", () => {
       assert.ok(filesResult.length !== 0)
     }
   })
+
+  it("validate sass", async () => {
+    const filePaths = [path.join(__dirname, "./fixtures/sass-test.scss")]
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, "../index.js"),
+      files: filePaths,
+      fix: false,
+    })
+
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || "[]") || []
+      filesResult.forEach(filesResult => {
+        console.log(`========== ${filesResult.filePath} ==========`);
+        console.log(filesResult.warnings);
+      });
+
+      assert.ok(filesResult.length !== 0)
+    }
+  })
+
+  it('Validate less', async () => {
+    const filePaths = [path.join(__dirname, './fixtures/less-test.less')];
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+
+      assert.ok(filesResult.length !== 0);
+    }
+  });
+
+  it('Validate css-module', async () => {
+    const filePaths = [path.join(__dirname, './fixtures/css-module.scss')];
+
+    const result = await stylelint.lint({
+      configFile: path.join(__dirname, '../index.js'),
+      files: filePaths,
+      fix: false,
+    });
+
+    if (result && result.errored) {
+      const filesResult = JSON.parse(result.output || '[]') || [];
+      filesResult.forEach((fileResult) => {
+        console.log(`========= ${filePaths} ==========`);
+        console.log(fileResult.warnings);
+      });
+
+      assert.ok(filesResult.length === 0);
+    }
+  });
 })
