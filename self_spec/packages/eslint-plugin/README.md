cc835059f590d1ba2043fafe0bb3d7bee567c0f9
xiongshengzhi <1220718650@qq.com>
2025-02-25 20:47:25 +0800

    [feat] 使用lerna添加eslint工具

---
+++ self_spec/packages/eslint-plugin/README.md
@@ -0,0 +1,11 @@
+# `@self_spec/eslint-plugin`
+
+> TODO: description
+
+## Usage
+
+```
+const eslintPlugin = require('@self_spec/eslint-plugin');
+
+// TODO: DEMONSTRATE API
+```
---
+++ self_spec/packages/eslint-plugin/rules/no-http-url.js
@@ -0,0 +1,27 @@
+const RULE_NAME = "no-http-url";
+
+module.exports = {
+  name: RULE_NAME,
+  meta: {
+    type: "suggestion",
+    fixable: null,
+    messages: {
+      noHttpUrl: 'Recommended "{{url}}" switch to HTTPS',
+    }
+  },
+  create(context) {
+    return {
+      Literal: function handleRequires(node) {
+        if (node.value && typeof node.value === "string" && node.value.startsWith("http://")) {
+          context.report({
+            node,
+            messageId: "noHttpUrl",
+            data: {
+              url: node.value
+            }
+          })
+        }
+      }
+    }
+  }
+}
